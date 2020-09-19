d5e9cc32a655b09b1d256b4c867be5758a04d12c
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-09-20 00:30:01 +0300

    added accordion component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { AccordionItem } from './components/accordion/accordion-item.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  
+  public accordionItems: AccordionItem[] = [
+    {
+      title: 'Awesome title',
+      content: 'This is an awesome component codex',
+      isExpanded: false,
+    },
+    {
+      title: 'The road to seniority',
+      content: 'Starts with a accordion component',
+      isExpanded: false,
+    },
+  ];
 }
