538693c98f9ceba1e600d1dce161b56004a65d83
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-09-29 21:55:55 +0300

    added credit card input component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -8,17 +8,21 @@ import { RouterModule } from '@angular/router';
 import { COMPONENTS_ROUTES } from './component.routes';
 import { StarRatingsComponent } from './star-ratings/star-ratings.component';
 import { ComponentDocumentationComponent } from './component-documentation/component-documentation.component';
+import { CreditCardInputComponent } from './credit-card-input/credit-card-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ComponentDocumentationComponent,
     StarRatingsComponent,
     AccordionComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    CreditCardInputComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
+    FormsModule,
     RouterModule.forChild(COMPONENTS_ROUTES)
   ]
 })
