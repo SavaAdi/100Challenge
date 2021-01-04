069a7548b282dff1d79d54a7f1970b2b4d8b198f
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-01-04 21:51:36 +0200

    Implemented route params with best practices

--- src/app/services/service-documentation/service-documentation.component.ts
+++ src/app/services/service-documentation/service-documentation.component.ts
@@ -3,6 +3,9 @@ import { SnackbarService } from '../snackbar/snackbar.service';
 import { User } from '../user/user.model';
 import { UserService } from '../user/user.service';
 import { LocalStorageService } from '../local-storage/local-storage.service';
+import { ActivatedRoute } from '@angular/router';
+
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-service-documentation',
@@ -12,13 +15,25 @@ import { LocalStorageService } from '../local-storage/local-storage.service';
 export class ServiceDocumentationComponent implements OnInit {
 
   public user = new User();
+  public userIdExample = new User();
   public state: { [key: string]: any } = {};
 
   constructor(public userService: UserService,
               public snackbarService: SnackbarService,
-              public localStorageService: LocalStorageService) { }
+              public localStorageService: LocalStorageService,
+              public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.route.params
+    .pipe(
+      map((params) => params.id),
+      mergeMap((id) => this.userService.getUserById(id))
+    ).subscribe({
+      next: (value) => {
+        this.userIdExample = value;
+      }
+    });
 
     this.localStorageService.state$.subscribe((data) => {
       this.state = data;
